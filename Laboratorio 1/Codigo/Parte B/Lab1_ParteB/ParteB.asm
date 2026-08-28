b71cabaa6890ab81eaff8905a6173422abd19f77
candelavargas-byte <candela.vargas@estudiantes.utec.edu.uy>
2026-08-28 17:48:20 -0300

    Funciones de los botones

--- Laboratorio 1/Codigo/Parte B/Lab1_ParteB/ParteB.asm
+++ Laboratorio 1/Codigo/Parte B/Lab1_ParteB/ParteB.asm
@@ -1,27 +1,48 @@
-.include "m328pdef.inc"
-.org 0x00
-.cseg
 
-    ldi r16, 0x3F ; Numero 0
-    ldi r17, 0x06 ; Numero 1
-    ldi r18, 0x5B ; Numero 2
-    ldi r19, 0x4F ; Numero 3
-    ldi r20, 0x66 ; Numero 4
-    ldi r21, 0x6D ; Numero 5
-    ldi r22, 0x7D ; Numero 6
-    ldi r23, 0x07 ; Numero 7
-    ldi r24, 0x7F ; Numero 8
-    ldi r25, 0x6F ; Numero 9
+	loop:
+    ;  Lectura de Botones
+    sbis PINB, 0       ; Si se presiona PB0 (Incremento)
+    rjmp boton_inc
 
-    ldi r26, 0xFF
-    out DDRD, r26      ; Puerto D como SALIDA (Display)
+    sbis PINB, 1       ; Si se presiona PB1 (Decremento)
+    rjmp boton_dec
 
-    clr r26
-    out DDRB, r26      ; Puerto B como ENTRADA (Botones)
-    
-    ldi r26, 0x07
-    out PORTB, r26     ; Activa Pull-Up en PB0, PB1 y PB2
+    sbis PINB, 2       ; Si se presiona PB2 (Reset)
+    rjmp boton_reset
 
-    ; --- Inicializar nuestro contador en r29 ---
-    ldi r29, 0         ; r29 sera nuestro contador del 0 al 9
-    rcall actualizar_display ; Llamamos a la rutina para que muestre el 0 al inicio
+    rjmp loop          ; Volver a revisar si no hay nada presionado
+
+; Botones
+boton_inc:
+    cpi r29, 9         ; Compara si el contador ya llegó a 9
+    breq esperar_soltar_inc ; Si es igual a 9, no incrementa más
+    inc r29            ; Incrementa el contador (le suma 1)
+    rcall actualizar_display
+
+esperar_soltar_inc:
+    sbis PINB, 0       ; Espera a que el botón se suelte (vuelva a 1)
+    rjmp esperar_soltar_inc
+    rcall retardo      ; Retardo para eliminar rebote al soltar
+    rjmp loop
+
+boton_dec:
+    cpi r29, 0         ; Compara si el contador ya está en 0
+    breq esperar_soltar_dec ; Si es igual a 0, no decrementa más
+    dec r29            ; Decrementa el contador (le resta 1)
+    rcall actualizar_display
+
+esperar_soltar_dec:
+    sbis PINB, 1       ; Espera a que el botón se suelte
+    rjmp esperar_soltar_dec
+    rcall retardo
+    rjmp loop
+
+boton_reset:
+    ldi r29, 0         ; Vuelve el contador a 0
+    rcall actualizar_display
+
+esperar_soltar_reset:
+    sbis PINB, 2       ; Espera a que el botón se suelte
+    rjmp esperar_soltar_reset
+    rcall retardo
+    rjmp loop
