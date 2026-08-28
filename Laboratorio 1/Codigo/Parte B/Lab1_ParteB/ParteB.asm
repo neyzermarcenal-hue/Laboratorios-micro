ae157974d845e25e360d6d712d593fd823d053f1
candelavargas-byte <candela.vargas@estudiantes.utec.edu.uy>
2026-08-28 17:53:30 -0300

    Modificación final del antirrebote y retardo debido a que en la simulación no se lograba apreciar bien el cambio de numeros

--- Laboratorio 1/Codigo/Parte B/Lab1_ParteB/ParteB.asm
+++ Laboratorio 1/Codigo/Parte B/Lab1_ParteB/ParteB.asm
@@ -1,11 +1,15 @@
-;Antirreobte y retardos
+;Antirrebote y Retardos 
 retardo:
-    ldi r27, 255
+    ldi r27, 20          ; Bucle exterior principal
 delay_ext:
-    ldi r28, 255
+    ldi r28, 200         ; Bucle intermedio
+delay_mid:
+    ldi r30, 250         ; Bucle interior
 delay_int:
-    dec r28
+    dec r30
     brne delay_int
+    dec r28
+    brne delay_mid
     dec r27
     brne delay_ext
     ret
