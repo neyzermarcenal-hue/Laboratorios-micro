76b68c94d968dfdc25be6041c8e613d76d580b22
candelavargas-byte <candela.vargas@estudiantes.utec.edu.uy>
2026-09-22 17:37:18 -0300

    Configuración USART y primera prueba de transmisión

--- Laboratorio 2/Codigo/Problema D/CodigoEmisor.asm
+++ Laboratorio 2/Codigo/Problema D/CodigoEmisor.asm
@@ -20,3 +20,34 @@ main:
     andi r16, ~((1<<2) | (1<<3) | (1<<4)) ; Desactivar pull-ups (asume pull-down externas)
     out PORTD, r16
 
+	;Configuracion USART
+    clr r16
+    sts UBRR0H, r16
+
+    ldi r16, 103
+    sts UBRR0L, r16
+
+    ; Habilitar ÚNICAMENTE Transmisor
+    ldi r16, (1 << TXEN0)
+    sts UCSR0B, r16
+
+    ; 8 bits de datos, 1 bit de parada
+    ldi r16, (1 << UCSZ01) | (1 << UCSZ00)
+    sts UCSR0C, r16
+
+;Leer y enviar
+loop_tx:
+    ; Primera prueba: Leer el puerto D completo y enviarlo
+    in r16, PIND
+
+wait_tx:
+    ; Esperar a que el buffer de transmisión esté listo
+    lds r17, UCSR0A
+    sbrs r17, UDRE0
+    rjmp wait_tx
+
+    ; Transmitir el valor crudo
+    sts UDR0, r16
+
+    rjmp loop_tx
+
