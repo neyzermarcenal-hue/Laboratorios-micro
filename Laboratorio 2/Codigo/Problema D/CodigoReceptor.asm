44994ae2c113785842d2a6f93797db9801802893
candelavargas-byte <candela.vargas@estudiantes.utec.edu.uy>
2026-09-22 18:16:00 -0300

    Comunicación USART y bucle de recepción

--- Laboratorio 2/Codigo/Problema D/CodigoReceptor.asm
+++ Laboratorio 2/Codigo/Problema D/CodigoReceptor.asm
@@ -22,3 +22,35 @@ main:
     clr r16
     out PORTC, r16
     out PORTB, r16
+
+	;Baud rate
+    clr r16
+    sts UBRR0H, r16
+
+    ldi r16, 103
+    sts UBRR0L, r16
+
+    ; Habilitar únicamente la recepción (RXEN0)
+    ldi r16, (1 << RXEN0)
+    sts UCSR0B, r16
+
+    ; Configurar marco: 8 bits de datos, 1 bit de parada
+    ldi r16, (1 << UCSZ01) | (1 << UCSZ00)
+    sts UCSR0C, r16
+
+;Bucle Principal
+loop_rx:
+
+wait_rx:
+    ; Esperar a que el bit RXC0 indique un dato entrante disponible
+    lds r17, UCSR0A
+    sbrs r17, RXC0
+    rjmp wait_rx
+
+    ; Leer el dato recibido desde el registro de datos UDR0
+    lds r16, UDR0
+
+    ; Filtrar la máscara: conservar los 3 bits inferiores (0 a 7)
+    andi r16, 0x07
+
+    rjmp loop_rx
