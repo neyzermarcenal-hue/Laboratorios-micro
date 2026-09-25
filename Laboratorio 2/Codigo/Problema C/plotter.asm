9eb72c1650862e1995225c9b5af6b4149070c016
Jmelendrez <julia.melendrez@estudiantes.utec.edu.uy>
2026-09-25 01:01:18 -0300

    Implementación de rutinas básicas de movimiento del plotter
    Se agregaron rutinas para controlar el solenoide del lápiz
    y los movimientos arriba, abajo, izquierda y derecha.
    También se incorporaron retardos iniciales para permitir
    la posterior calibración con el plotter físico.

--- Laboratorio 2/Codigo/Problema C/plotter.asm
+++ Laboratorio 2/Codigo/Problema C/plotter.asm
@@ -41,3 +41,125 @@ main:
 loop:
 
     rjmp loop
+
+
+;==================================================
+; CONTROL DEL LAPIZ
+;==================================================
+
+lapiz_abajo:
+
+    sbi PORTD, PD2       ; Activa bajar solenoide
+    rcall delay_rele
+    cbi PORTD, PD2       ; Desactiva salida
+
+    ret
+
+
+lapiz_arriba:
+
+    sbi PORTD, PD3       ; Activa subir solenoide
+    rcall delay_rele
+    cbi PORTD, PD3       ; Desactiva salida
+
+    ret
+
+
+;==================================================
+; MOVIMIENTOS
+;==================================================
+
+mover_abajo:
+
+    sbi PORTD, PD4
+    rcall delay_movimiento
+    cbi PORTD, PD4
+
+    ret
+
+
+mover_arriba:
+
+    sbi PORTD, PD5
+    rcall delay_movimiento
+    cbi PORTD, PD5
+
+    ret
+
+
+mover_izquierda:
+
+    sbi PORTD, PD6
+    rcall delay_movimiento
+    cbi PORTD, PD6
+
+    ret
+
+
+mover_derecha:
+
+    sbi PORTD, PD7
+    rcall delay_movimiento
+    cbi PORTD, PD7
+
+    ret
+
+
+;==================================================
+; RETARDO PARA MOVIMIENTO
+;==================================================
+
+delay_movimiento:
+
+    ldi r18, 20
+
+delay_mov_ext:
+
+    ldi r19, 255
+
+delay_mov_med:
+
+    ldi r20, 255
+
+delay_mov_int:
+
+    dec r20
+    brne delay_mov_int
+
+    dec r19
+    brne delay_mov_med
+
+    dec r18
+    brne delay_mov_ext
+
+    ret
+
+
+;==================================================
+; RETARDO DEL RELE / SOLENOIDE
+;==================================================
+
+delay_rele:
+
+    ldi r18, 10
+
+delay_rele_ext:
+
+    ldi r19, 255
+
+delay_rele_med:
+
+    ldi r20, 255
+
+delay_rele_int:
+
+    dec r20
+    brne delay_rele_int
+
+    dec r19
+    brne delay_rele_med
+
+    dec r18
+    brne delay_rele_ext
+
+    ret
