8b3f14fb209e00462487a850be6f710b9635c630
Jmelendrez <julia.melendrez@estudiantes.utec.edu.uy>
2026-09-25 03:33:11 -0300

    Refactorizar la estructura de datos MENU en plotter.asm

--- Laboratorio 2/Codigo/Problema C/plotter.asm
+++ Laboratorio 2/Codigo/Problema C/plotter.asm
@@ -1333,8 +1333,4 @@ delay_rele_int:
 
 MENU:
 
-    .db 13,10,"PLOTTER UTEC",13,10
-    .db "1 - Triangulo",13,10,"2 - Circulo",13,10
-    .db "3 - Pentagrama",13,10,"4 - Figura libre",13,10
-    .db "P - Gengar",13,10,"T - Todas",13,10
-    .db "Seleccione: ",0,0
+ .db 13,10,"PLOTTER UTEC",13,10,"1 - Triangulo",13,10,"2 - Circulo",13,10,"3 - Pentagrama",13,10,"4 - Figura libre",13,10,"P - Gengar",13,10,"T - Todas",13,10,"Seleccione: ",0
